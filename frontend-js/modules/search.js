5fac1f772fef0ee102a3b91cd24936c6a73aac76
Nanocika <125473913+Nanocika@users.noreply.github.com>
2023-03-28 15:19:23 +0200

    Update search.js

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -4,7 +4,6 @@ export default class Search {
   // 1. Select DOM elements, and keep track of any useful data
   constructor() {
     this.injectHTML()
-    
     this.headerSearchIcon = document.querySelector(".header-search-icon")
     this.overlay = document.querySelector(".search-overlay")
     this.closeIcon = document.querySelector(".close-live-search")
@@ -16,7 +15,6 @@ export default class Search {
     this.events()
   }
   // 2. Events
-
   events() {
     this.inputField.addEventListener("keyup", ()=> this.keyPressHandler())
     this.closeIcon.addEventListener("click", () => this.closeOverlay())
@@ -26,30 +24,114 @@ export default class Search {
     })
   }
   // 3. Methods
-  //ha elengedi a billenyut (p) akkor indul el a methodus, elindul az kerek icon, es elkezd 3 mp varni es csak utan indul a serverre a request
-  //de ha 3 mp alatt elenged egy ujabb billenytyut akkor nem varja meg az idozitot , hanem  indul a methodus ujra es nulazza az eddig idozito
-   //es a send request csak akkor indul ha 3 mp utan se nyom le ujabb billenyut a felhasznalo    
+  
+  /*ha elengedi a billenyut (p) akkor indul el a methodus, elindul az kerek icon, es elkezd 3 mp varni es csak utan indul a serverre a request
+  de ha 3 mp alatt elenged egy ujabb billenytyut akkor nem varja meg az idozitot , hanem  indul a methodus ujra es nulazza az eddig idozito
+  es a send request csak akkor indul ha 3 mp utan se nyom le ujabb billenyut a felhasznalo  */  
+  
    keyPressHandler() {
     let value = this.inputField.value
+    
+    if (value == "") {
+      clearTimeout(this.typingWaitimer)
+      this.hideLoaderIcon()
+      this.hideResultsArea()
+    }
+
     if (value != "" && value != this.previousValue){
       clearTimeout(this.typingWaitimer)//clears to zero
       this.showLoaderIcon()
-      this.typingWaitimer = setTimeout(()=>this.sendRequest(), 3000)
+      this.hideResultsArea()
+      this.typingWaitimer = setTimeout(()=>this.sendRequest(), 750)
     }
     this.previousValue = value
   }
+
+ // ez a methodus meg csak annyit csinal hogy a kereses sikeres talalatakor kiirja a bongeszo
+ // konzoljara a nyers JSON adatot (egy tomb a postokkal amiben szerepel az input. value vagy a bodyban vagy a titleben)
+ // + renderel egy statikus HTMLT (ebbe kesoobb JS -el beleirjuk a helyes postot a tombol)
+ // sikertelenseg-kor  kiir egy hibauzit  
  sendRequest() {
       axios.post('/search', {searchTerm: this.inputField.value}).then(response=>{
         
         
         console.log(response.data)
+        this.renderResultsHTML(response.data)
       }).catch(()=>{
         alert("Hello, the request failed.")
       })
   }
-   showLoaderIcon() {
+
+
+  renderResultsHTML(posts){
+    if (posts.length){
+      this.resultsArea.innerHTML = `          <div class="list-group shadow-sm">
+
+      <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
+
+
+
+      <a href="#" class="list-group-item list-group-item-action">
+
+        <img class="avatar-tiny" src=https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128> <strong>Example Post #1</strong>
+
+        <span class="text-muted small">by barksalot on 0/14/2019</span>
+
+      </a>
+
+      <a href="#" class="list-group-item list-group-item-action">
+
+        <img class="avatar-tiny" src=https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128> <strong>Example Post #2</strong>
+
+        <span class="text-muted small">by brad on 0/12/2019</span>
+
+      </a>
+
+      <a href="#" class="list-group-item list-group-item-action">
+
+        <img class="avatar-tiny" src=https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128> <strong>Example Post #3</strong>
+
+        <span class="text-muted small">by barksalot on 0/14/2019</span>
+
+      </a>
+
+      <a href="#" class="list-group-item list-group-item-action">
+
+        <img class="avatar-tiny" src=https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128> <strong>Example Post #4</strong>
+
+        <span class="text-muted small">by brad on 0/12/2019</span>
+
+      </a>
+
+    </div>`
+    } else {
+      this.resultsArea.innerHTML = `<p class = "alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search</p>`
+    }
+    
+    this.hideLoaderIcon()
+    this.showResultsArea()
+
+
+  }
+
+  showLoaderIcon() {
     this.loaderIcon.classList.add("circle-loader--visible")
   }
+  
+  hideLoaderIcon() {
+    this.loaderIcon.classList.remove("circle-loader--visible")
+  }
+  
+  showResultsArea(){
+    this.resultsArea.classList.add("live-search-results--visible")
+
+  }
+
+  hideResultsArea(){
+    this.resultsArea.classList.remove("live-search-results--visible")
+
+  }
+  
   openOverlay() {
     this.overlay.classList.add("search-overlay--visible")
     setTimeout(()=>this.inputField.focus(), 50)
@@ -86,49 +168,7 @@ export default class Search {
 
         <div class="circle-loader"></div>
 
-        <div class="live-search-results">
-
-          <div class="list-group shadow-sm">
-
-            <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
-
- 
-
-            <a href="#" class="list-group-item list-group-item-action">
-
-              <img class="avatar-tiny" src=https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128> <strong>Example Post #1</strong>
-
-              <span class="text-muted small">by barksalot on 0/14/2019</span>
-
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action">
-
-              <img class="avatar-tiny" src=https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128> <strong>Example Post #2</strong>
-
-              <span class="text-muted small">by brad on 0/12/2019</span>
-
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action">
-
-              <img class="avatar-tiny" src=https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128> <strong>Example Post #3</strong>
-
-              <span class="text-muted small">by barksalot on 0/14/2019</span>
-
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action">
-
-              <img class="avatar-tiny" src=https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128> <strong>Example Post #4</strong>
-
-              <span class="text-muted small">by brad on 0/12/2019</span>
-
-            </a>
-
-          </div>
-
-        </div>
+        <div class="live-search-results"></div>
 
       </div>
 
