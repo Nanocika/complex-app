364f29fff5e469ecb97fb1ccc4a05c6d42c40fd1
Nanocika <125473913+Nanocika@users.noreply.github.com>
2023-03-30 14:19:38 +0200

    Csutortoki valtoztatasok

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -29,7 +29,7 @@ export default class Search {
   de ha 3 mp alatt elenged egy ujabb billenytyut akkor nem varja meg az idozitot , hanem  indul a methodus ujra es nulazza az eddig idozito
   es a send request csak akkor indul ha 3 mp utan se nyom le ujabb billenyut a felhasznalo  */  
   
-   keyPressHandler() {
+  keyPressHandler() {
     let value = this.inputField.value
     
     if (value == "") {
@@ -65,45 +65,21 @@ export default class Search {
 
   renderResultsHTML(posts){
     if (posts.length){
-      this.resultsArea.innerHTML = `          <div class="list-group shadow-sm">
+      this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
+      <div class="list-group-item active"><strong>Search Results</strong> ${posts.length >1 ? `${posts.length} (items found)` : `(1 item found)`}</div>
+      ${posts.map(post=>{
+        let postDate = new Date(post.createdDate)
 
-      <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
+        return `<a href="/post/${post._id}" class="list-group-item list-group-item-action">
 
+        <img class="avatar-tiny" src="${post.author.avatar}" <strong>${post.title}</strong>
 
+        <span class="text-muted small">by ${post.author.username} on ${postDate.getMonth()+1}/${postDate.getDate()}/${postDate.getFullYear()}</span>
 
-      <a href="#" class="list-group-item list-group-item-action">
-
-        <img class="avatar-tiny" src=https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128> <strong>Example Post #1</strong>
-
-        <span class="text-muted small">by barksalot on 0/14/2019</span>
-
-      </a>
-
-      <a href="#" class="list-group-item list-group-item-action">
-
-        <img class="avatar-tiny" src=https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128> <strong>Example Post #2</strong>
-
-        <span class="text-muted small">by brad on 0/12/2019</span>
-
-      </a>
-
-      <a href="#" class="list-group-item list-group-item-action">
-
-        <img class="avatar-tiny" src=https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128> <strong>Example Post #3</strong>
-
-        <span class="text-muted small">by barksalot on 0/14/2019</span>
-
-      </a>
-
-      <a href="#" class="list-group-item list-group-item-action">
-
-        <img class="avatar-tiny" src=https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128> <strong>Example Post #4</strong>
-
-        <span class="text-muted small">by brad on 0/12/2019</span>
-
-      </a>
-
-    </div>`
+      </a>`
+      }).join("")}
+      
+      </div>`
     
   } else {
       this.resultsArea.innerHTML = `<p class = "alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search</p>`
@@ -136,8 +112,8 @@ export default class Search {
   }
   closeOverlay() {
     this.overlay.classList.remove("search-overlay--visible")
-    this.inputField.value = ""
   }
+    
   injectHTML() {
     document.body.insertAdjacentHTML('beforeend', `<div class="search-overlay">
 
