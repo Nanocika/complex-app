9bc19d11c04292530ecd2d6f0d196013ba2a0f1b
Nanocika <125473913+Nanocika@users.noreply.github.com>
2023-06-23 15:08:18 +0200

    2023.06.23 Dynamic HTML Title

--- controllers/userController.js
+++ controllers/userController.js
@@ -127,6 +127,7 @@ exports.profilePostsScreen =  function(req, res,) {
     //ask our post model for multiple posts by a certain author id
     Post.findByAuthorId(req.profileUser._id).then(function(posts) {
         res.render('profile', {
+            title: `Profile for ${req.profileUser.username}`,
             currentPage: "posts", 
             posts:posts,
             profileUsername: req.profileUser.username,
@@ -146,6 +147,7 @@ exports.profileFollowersScreen = async function (req, res) {
    try {
     let followers = await Follow.getFollowersById(req.profileUser._id)
     res.render('profile-followers', {
+        title: `Profile for ${req.profileUser.username}`,
         currentPage: "followers",  
         followers: followers,
         profileUsername: req.profileUser.username,
