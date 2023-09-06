e1f8c34fba2dd90c85226abc2c264e831ca9e278
Nanocika <125473913+Nanocika@users.noreply.github.com>
2023-09-06 17:54:13 +0200

    2023.09.06

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const MongoStore = require("connect-mongo")
 const flash = require("connect-flash")
 const markdown = require("marked")
 const sanitizeHTML = require("sanitize-html")
+
+const csrf = require("csurf")
+
 const app = express()
+
 let sessionOptions = session({
   secret: "JavaScript is sooooooooooo cool",
   store: MongoStore.create({ client: require("./db") }),
@@ -40,7 +44,29 @@ app.use(express.json())
 app.use(express.static("public"))
 app.set("views", "views")
 app.set("view engine", "ejs")
+
+app.use(csrf())
+
+app.use(function(req, res, next){
+  res.locals.csrfToken = req.csrfToken()
+  next()
+})
+
 app.use("/", router)
+
+
+app.use(function(err, req, res, next){
+  if(err){
+    if (err.code == "EBADCSRFTOKEN") {
+      req.flash('errors', "Cross site request forgery detected!!!")
+      req.session.save(()=>res.redirect('/'))
+    } else {
+      res.redirect("404")
+    }
+
+  }
+})
+
 
 //express webserver added to server
 const server = require("http").createServer(app)
